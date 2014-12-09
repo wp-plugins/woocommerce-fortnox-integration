6537990a22e5b5b7828bd1f07c9a6214ed36baee
pierrewiberg <pierrewiberg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-09 11:41:28 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-fortnox-integration/tags/1.37@1041125 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class-fortnox3-order-xml.php
+++ class-fortnox3-order-xml.php
@@ -41,10 +41,27 @@ class WCF_Order_XML_Document extends WCF_XML_Document{
         $order['DeliveryCity'] = $arr->shipping_city;
         $order['DeliveryCountry'] = $this->countries[$arr->shipping_country];
         $order['DeliveryZipCode'] =  $arr->shipping_postcode;
-        $order['CustomerName'] = $arr->billing_first_name . " " . $arr->billing_last_name;
-        $order['DeliveryName'] = $arr->billing_first_name . " " . $arr->billing_last_name;
-        $order['VATIncluded'] = 'true';
 
+        if(isset($arr->billing_company)){
+            $order['CustomerName'] = $arr->billing_first_name . " " . $arr->billing_last_name . " " . $arr->billing_company;
+        }
+        else{
+            $order['DeliveryName'] = $arr->billing_first_name . " " . $arr->billing_last_name;
+        }
+
+        $order['VATIncluded'] = 'false';
+        if($options['activate-vat'] == 'on'){
+            $order['VATIncluded'] = 'true';
+        }
+        else{
+            $order['VATIncluded'] = 'false';
+        }
+
+
+        if($order_options['add-payment-type'] == 'on'){
+            $payment_method = get_post_meta( $arr->id, '_payment_method_title');
+            $order['Remarks'] = $payment_method[0];
+        }
         $email = array();
         $email['EmailAddressTo'] = $arr->billing_email;
         $order['EmailInformation'] =  $email;
@@ -68,6 +85,7 @@ class WCF_Order_XML_Document extends WCF_XML_Document{
             }
 
             $product = $pf->get_product($productId);
+            logthis(print_r($product, true));
             $invoicerow = array();
             $invoicerow['ArticleNumber'] = $product->get_sku();
             $invoicerow['Description'] = $item['name'];
@@ -75,24 +93,29 @@ class WCF_Order_XML_Document extends WCF_XML_Document{
             $invoicerow['DeliveredQuantity'] = $item['qty'];
             $invoicerow['OrderedQuantity'] = $item['qty'];
 
-            if($options['activate-vat'] == 'on'){
-                $invoicerow['Price'] = $product->get_price_including_tax();
-            }
-            else{
-                $invoicerow['Price'] = $product->get_price_excluding_tax();
-            }
+            $invoicerow['Price'] = $this->get_product_price($item);
 
-            $invoicerow['VAT'] = $item['tax_class'];
-            //discount
-            if($product->is_on_sale()){
-                $invoicerow['Discount'] = $item['qty']*($product->get_regular_price() - $product->get_sale_price());
-                $invoicerow['DiscountType'] = 'AMOUNT';
-            }
-
+            $invoicerow['VAT'] = $this->get_tax_class_by_tax_name($item['tax_class']);
             $index += 1;
             $invoicerows[$key] = $invoicerow;
         }
         $order['OrderRows'] = $invoicerows;
+
+        logthis(print_r($order, true));
+
         return $this->generate($root, $order);
+    }
+
+    private function get_product_price($product){
+        return floatval($product['line_total']);
+    }
+
+    public function get_tax_class_by_tax_name( $tax_name ) {
+        global $wpdb;
+        if($tax_name == ''){
+            return 25;
+        }
+        $tax_rate = $wpdb->get_var( $wpdb->prepare( "SELECT tax_rate FROM {$wpdb->prefix}woocommerce_tax_rates WHERE tax_rate_name = %d", $tax_name ) );
+        return intval($tax_rate);
     }
 }
